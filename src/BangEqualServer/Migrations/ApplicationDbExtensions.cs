4b3b0037e97b98ae81cc01c9564efc66e28ad2c6
Herman A. Toro <herman.toro@outlook.com>
2017-05-15 14:03:19 -0400

    Update ApplicationDbExtensions.cs

--- src/BangEqualServer/Migrations/ApplicationDbExtensions.cs
+++ src/BangEqualServer/Migrations/ApplicationDbExtensions.cs
@@ -11,32 +11,13 @@ namespace BareMetalApi.Migrations
     public static class ApplicationDbExtensions
     {
 
-        public static void EnsureSeedData(this ApplicationDbContext context, string jsonData)
+        public static void EnsureSeedData(this ApplicationDbContext context)
         {
-            List<Content> sitecontent = JsonConvert.DeserializeObject<List<Content>>(jsonData);
-
             if (!context.Database.GetPendingMigrations().Any())
             {
                 if (!context.SiteContent.Any())
                 {
-                    foreach(Content c in sitecontent)
-                    {
-                        context.SiteContent.AddRange(
-                            new Content {
-                            ContentId = c.ContentId, 
-                            Type = c.Type,
-                            Title = c.Title, 
-                            Author = c.Author, 
-                            Topic = c.Topic, 
-                            Tags = c.Tags, 
-                            Views = c.Views, 
-                            Shares = c.Shares,
-                            Caption = c.Caption,
-                            Active = c.Active, 
-                            RenderString = c.RenderString
-                            }); 
-                        context.SaveChanges();
-                    }    
+                  
                 }                   
             }
         }
